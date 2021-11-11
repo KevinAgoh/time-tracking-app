c5ea1565fc206199ad62c3b4326ee06c3955ef6b
Kevin Agoh <kevinagoh@hotmail.fr>
2021-11-11 15:11:31 +0100

    tabs display function is working now

--- src/index.js
+++ src/index.js
@@ -1,96 +1,68 @@
-const work = document.getElementById('work');
-const play = document.getElementById('play');
-const study = document.getElementById('study');
-const exercice = document.getElementById('exercice');
-const social = document.getElementById('social');
-const care = document.getElementById('care');
+const grid = document.querySelector('.activity-grid');
 
 fetch('../data.json')
 .then(response => response.json())
 .then((data) => {
-  const workContent = `
-    <div class="overview-box">
-      <div class="activity-overview">
-        <p>${data[0].title}</p>
-        <h1>${data[0].timeframes.daily.current}hrs</h1>
-        <p>Last Week - ${data[0].timeframes.daily.previous}</p>
+  const dailyGrid = `
+ 
+      <div id="work" class="windowcontent daily"> 
+        <div class="overview-box">
+          <div class="activity-overview">
+            <p>${data[0].title}</p>
+            <h1>${data[0].timeframes.daily.current}hrs</h1>
+            <p>Last Week - ${data[0].timeframes.daily.previous}</p>
+          </div>
+        </div>
       </div>
-      <div class="button">
-        <button>. . .</button>
+      <div id="play" class="windowcontent daily">
+        <div class="overview-box">
+          <div class="activity-overview">
+            <p>${data[1].title}</p>
+            <h1>${data[1].timeframes.daily.current}hrs</h1>
+            <p>Last Week - ${data[1].timeframes.daily.previous}</p>
+          </div>
+        </div>
       </div>
-      
-    </div>
+      <div id="study" class="windowcontent daily">
+        <div class="overview-box">
+          <div class="activity-overview">
+            <p>${data[2].title}</p>
+            <h1>${data[2].timeframes.daily.current}hrs</h1>
+            <p>Last Week - ${data[2].timeframes.daily.previous}</p>
+          </div>
+        </div>
+      </div>
+      <div id="exercice" class="windowcontent daily">
+        <div class="overview-box">
+          <div class="activity-overview">
+            <p>${data[3].title}</p>
+            <h1>${data[3].timeframes.daily.current}hrs</h1>
+            <p>Last Week - ${data[3].timeframes.daily.previous}</p>
+          </div>
+        </div>
+      </div>
+      <div id="social" class="windowcontent daily">
+        <div class="overview-box">
+          <div class="activity-overview">
+            <p>${data[4].title}</p>
+            <h1>${data[4].timeframes.daily.current}hrs</h1>
+            <p>Last Week - ${data[4].timeframes.daily.previous}</p>
+          </div>
+        </div>
+      </div>
+      <div id="care" class="windowcontent daily">
+        <div class="overview-box">
+          <div class="activity-overview">
+            <p>${data[5].title}</p>
+            <h1>${data[5].timeframes.daily.current}hrs</h1>
+            <p>Last Week - ${data[5].timeframes.daily.previous}</p>
+          </div>
+        </div>
+      </div>
+   
   `;
 
-  const playContent = `
-    <div class="overview-box">
-      <div class="activity-overview">
-        <p>${data[1].title}</p>
-        <h1>${data[1].timeframes.daily.current}hrs</h1>
-        <p>Last Week - ${data[1].timeframes.daily.previous}</p>
-      </div>
-      <div class="button">
-        <button>. . .</button>
-      </div>
-    </div>
-  `;
-
-  const studyContent = `
-    <div class="overview-box">
-      <div class="activity-overview">
-        <p>${data[2].title}</p>
-        <h1>${data[2].timeframes.daily.current}hrs</h1>
-        <p>Last Week - ${data[2].timeframes.daily.previous}</p>
-      </div>
-      <div class="button">
-        <button>. . .</button>
-      </div>
-    </div>
-  `;
-
-  const exerciceContent = `
-    <div class="overview-box">
-      <div class="activity-overview">
-        <p>${data[3].title}</p>
-        <h1>${data[3].timeframes.daily.current}hrs</h1>
-        <p>Last Week - ${data[3].timeframes.daily.previous}</p>
-      </div>
-      <div class="button">
-        <button>. . .</button>
-      </div>
-    </div>
-  `;
-  const socialContent = `
-    <div class="overview-box">
-      <div class="activity-overview">
-        <p>${data[4].title}</p>
-        <h1>${data[4].timeframes.daily.current}hrs</h1>
-        <p>Last Week - ${data[4].timeframes.daily.previous}</p>
-      </div>
-      <div class="button">
-        <button>. . .</button>
-      </div>
-    </div>
-  `;
-  const careContent = `
-    <div class="overview-box">
-      <div class="activity-overview">
-        <p>${data[5].title}</p>
-        <h1>${data[5].timeframes.daily.current}hrs</h1>
-        <p>Last Week - ${data[5].timeframes.daily.previous}</p>
-      </div>
-      <div class="button">
-        <button>. . .</button>
-      </div>
-    </div>
-  `;
-  
-  work.insertAdjacentHTML('beforeend', workContent);
-  play.insertAdjacentHTML('beforeend', playContent);
-  study.insertAdjacentHTML('beforeend', studyContent);
-  exercice.insertAdjacentHTML('beforeend', exerciceContent);
-  social.insertAdjacentHTML('beforeend', socialContent);
-  care.insertAdjacentHTML('beforeend', careContent);
+  grid.insertAdjacentHTML('beforeend', dailyGrid);
 });
 
 function openWindow(evt, windowName) {
@@ -106,7 +78,9 @@ function openWindow(evt, windowName) {
     windowLinks[i].className = windowLinks[i].className.replace( ' active', '');
   }
 
-  document.getElementById(windowName).style.display = 'block';
+  document.querySelectorAll(windowName).forEach((div) => {
+    div.style.display = 'flex';
+  })
   evt.currentTarget.className += ' active';
 }
 
